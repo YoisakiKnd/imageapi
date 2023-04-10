72a4dfc8da0d0cb66fe5fa970d7fe16f75854bd9
YoisakiKnd <88635459+YoisakiKnd@users.noreply.github.com>
2023-04-11 07:58:27 +0800

    upload php files

---
+++ random.php
@@ -0,0 +1,7 @@
+<?php
+$img_array = glob('D2PE-IMG/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
+if(count($img_array) == 0) die('没有找到图片文件。MuaOoO ~ '.dirname(__FILE__).'/D2PE-IMG/ 文件夹');
+header('Content-Type: image/png');
+echo(file_get_contents($img_array[array_rand($img_array)]));
+?>
+#需要注意的地方在代码的第四行：header('Content-Type: image/png');，这里输入的图片类型，根据直接的图片格式来写。
